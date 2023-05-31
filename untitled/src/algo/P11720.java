1b304c5dde7313c7df697ed65dab6b76128efdc6
refresh88 <ouok88@gmail.com>
2023-06-01 00:20:30 +0900

    algo 1day
    알고리즘 달리기 시작

---
+++ untitled/src/algo/P11720.java
@@ -0,0 +1,19 @@
+package algo;
+
+import java.util.Scanner;
+
+public class P11720 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int givenNumber = scan.nextInt();
+
+        String addNeedNumber = scan.next();
+        char[] dividedNumber = addNeedNumber.toCharArray();
+        int sum = 0;
+        for(int i=0; i < dividedNumber.length; i++) {
+            sum += dividedNumber[i] - '0';
+        }
+        System.out.println(sum);
+
+    }
+}
